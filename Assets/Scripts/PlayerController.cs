238a866c3da8d9f9960a92b5bf356403de4ee14e
Emerson_Scalcon <198485@upf.br>
2025-11-03 22:06:55 -0300

    update: projeto Unity (Delimitação do espaço de jogo)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,54 +1,3 @@
-/*
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    public float speed;         //Floating point variable to store the player's movement speed.
-
-    private Rigidbody2D rb2d;   //Store a reference to the Rigidbody2D component required to use 2D Physics.
-	
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D> ();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-	
-    // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-    void FixedUpdate()
-    {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
-    }
-	
-    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
-    void OnTriggerEnter2D(Collider2D other) 
-    {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            other.gameObject.SetActive(false);
-        }
-    }	
-}
-
-//teste
-*/
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -109,7 +58,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        ScreenWrap();
+        //ScreenWrap();
     }
 
     void FixedUpdate()
@@ -124,7 +73,7 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
     }
 
-    void ScreenWrap()
+    /*void ScreenWrap()
     {
         var cam = Camera.main;
         var v = cam.WorldToViewportPoint(body.position);
@@ -132,5 +81,5 @@ public class PlayerController : MonoBehaviour
         if (v.y > 1f) v.y = 0f; else if (v.y < 0f) v.y = 1f;
         var w = cam.ViewportToWorldPoint(v);
         body.position = new Vector3(w.x, w.y, 0f);
-    }
+    }*/
 }
